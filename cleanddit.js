bfea8142420a80f8df21274482c9b9d77b4e8362
David García <daganu@gmail.com>
2024-01-14 19:33:31 +0100

    Update cleanddit.js

--- cleanddit.js
+++ cleanddit.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cleanddit
 // @namespace    http://tampermonkey.net/
-// @version      0.7
+// @version      0.8
 // @description  Remove some annoying things
 // @author       deibit
 // @license      MIT
@@ -13,37 +13,45 @@
 // ==/UserScript==
 
 function removeElements() {
-  let promoted = document.body.getElementsByClassName("promotedlink");
-  for (var i = 0; i < promoted.length; i++) {
-    promoted[i].parentElement.removeChild(promoted[i]);
-  }
+    // Remove promoted post
+    let promoted = document.body.getElementsByClassName("promotedlink");
+    for (var i = 0; i < promoted.length; i++) {
+        promoted[i].parentElement.removeChild(promoted[i]);
+    }
 
-  if (document.location.href.includes("comments")) return;
-  const levels = 6;
-  let popular = document.body.getElementsByClassName("_1qeIAgB0cPwnLhDF9XSiJM");
-  for (i = 0; i < popular.length; i++) {
-    let popular_item = popular[i];
-    for (var j = 0; j < levels; j++) {
-      if (popular_item.parentElement) {
-        popular_item = popular_item.parentElement;
-      } else {
-        break;
-      }
+    // Remove popup for Notifications
+    let popups = document.querySelectorAll('[id^="popup"]');
+    for ( i = 0; i < popups.length; i++) {
+        popups[i].parentElement.removeChild(popups[i]);
     }
-    if (popular_item) {
-      console.log("deleting " + popular_item);
-      popular_item.parentElement.removeChild(popular_item);
+
+    // Remove non-subscribe related post
+    if (document.location.href.includes("comments")) return;
+    const levels = 6;
+    let popular = document.body.getElementsByClassName("_1qeIAgB0cPwnLhDF9XSiJM");
+    for (i = 0; i < popular.length; i++) {
+        let popular_item = popular[i];
+        for (var j = 0; j < levels; j++) {
+            if (popular_item.parentElement) {
+                popular_item = popular_item.parentElement;
+            } else {
+                break;
+            }
+        }
+        if (popular_item) {
+            console.log("deleting " + popular_item);
+            popular_item.parentElement.removeChild(popular_item);
+        }
     }
-  }
 }
 
 var MutationObserver = window.MutationObserver;
 var myObserver = new MutationObserver(removeElements);
 var obsConfig = {
-  childList: true,
-  attributes: true,
-  subtree: true,
-  attributeFilter: ["class"],
+    childList: true,
+    attributes: true,
+    subtree: true,
+    attributeFilter: ["class"],
 };
 
 myObserver.observe(document, obsConfig);
