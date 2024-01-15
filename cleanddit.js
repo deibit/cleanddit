99bcc7dff55e96a26ca54a42bdb9cb193918d629
David García <daganu@gmail.com>
2024-01-15 22:47:36 +0100

    Update cleanddit.js

--- cleanddit.js
+++ cleanddit.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cleanddit
 // @namespace    http://tampermonkey.net/
-// @version      0.8
+// @version      0.9
 // @description  Remove some annoying things
 // @author       deibit
 // @license      MIT
@@ -26,22 +26,29 @@ function removeElements() {
     }
 
     // Remove non-subscribe related post
-    if (document.location.href.includes("comments")) return;
-    const levels = 6;
-    let popular = document.body.getElementsByClassName("_1qeIAgB0cPwnLhDF9XSiJM");
-    for (i = 0; i < popular.length; i++) {
-        let popular_item = popular[i];
-        for (var j = 0; j < levels; j++) {
-            if (popular_item.parentElement) {
-                popular_item = popular_item.parentElement;
-            } else {
-                break;
+    if (!document.location.href.includes("comments") && false) {
+        const levels = 6;
+        let popular = document.body.getElementsByClassName("_1qeIAgB0cPwnLhDF9XSiJM");
+        for (i = 0; i < popular.length; i++) {
+            let popular_item = popular[i];
+            for (var j = 0; j < levels; j++) {
+                if (popular_item.parentElement) {
+                    popular_item = popular_item.parentElement;
+                } else {
+                    break;
+                }
+            }
+            if (popular_item) {
+                console.log("deleting " + popular_item);
+                popular_item.parentElement.removeChild(popular_item);
             }
         }
-        if (popular_item) {
-            console.log("deleting " + popular_item);
-            popular_item.parentElement.removeChild(popular_item);
-        }
+    }
+
+    // Remove "Create Post" popup
+    let createPostPopup = document.getElementsByClassName("_3q-XSJ2vokDQrvdG6mR__k");
+    if (createPostPopup.length > 0) {
+        createPostPopup[0].remove();
     }
 }
 
